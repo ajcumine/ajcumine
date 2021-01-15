ac291570ba288a7b29097a72ce11358b6439ed05
ajcumine <ajcumine@gmail.com>
2021-01-15 13:42:51 +0000

    Add reworked code from miles app project

---
+++ components/header-image.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 12rem;
+  height: 20rem;
+`;
+
+const HeaderImage = () => <Wrapper />;
+
+export default HeaderImage;
